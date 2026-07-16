ce8784a0c556d8f0b88cce448d617f9874e4355c
Stephan Krüger <stephan.krueger@acp.de>
2026-07-16 11:09:40 +0200

    new file:   Entra Connect/Export_Sync_Rules.ps1

---
+++ Entra Connect/Export_Sync_Rules.ps1
@@ -0,0 +1,36 @@
+$RuleScopeReport = foreach ($Rule in Get-ADSyncRule | Sort-Object Direction, Precedence) {
+
+    if ($null -ne $Rule.ScopeFilter -and $null -ne $Rule.ScopeFilter.ScopeConditionGroups) {
+
+        $GroupIndex = 0
+
+        foreach ($Group in $Rule.ScopeFilter.ScopeConditionGroups) {
+            $GroupIndex++
+
+            $ConditionIndex = 0
+
+            foreach ($Condition in $Group.ScopeConditions) {
+                $ConditionIndex++
+
+                [PSCustomObject]@{
+                    RuleName          = $Rule.Name
+                    RuleIdentifier    = $Rule.Identifier
+                    Direction         = $Rule.Direction
+                    Precedence        = $Rule.Precedence
+                    Disabled          = $Rule.Disabled
+                    Connector         = $Rule.Connector
+                    SourceObjectType  = $Rule.SourceObjectType
+                    TargetObjectType  = $Rule.TargetObjectType
+                    ScopeGroup        = $GroupIndex
+                    ConditionNumber   = $ConditionIndex
+                    Attribute         = $Condition.Attribute
+                    Operator          = $Condition.ComparisonOperator
+                    Value             = $Condition.ComparisonValue
+                }
+            }
+        }
+    }
+}
+
+$RuleScopeReport |
+Export-Csv "$ExportPath\EntraConnect_SyncRules_ScopeFilters.csv" -NoTypeInformation -Encoding UTF8
