4d2eef24127343d902c9da48ba0200a24174137b
Stephan Krüger <stephan.krueger@acp.de>
2026-07-16 11:33:47 +0200

    modified:   Entra Connect/Export_Sync_Rules.ps1

--- Entra Connect/Export_Sync_Rules.ps1
+++ Entra Connect/Export_Sync_Rules.ps1
@@ -2,13 +2,38 @@ $ExportPath = "C:\Temp\"
 New-Item -Path $ExportPath -ItemType Directory -Force
 
 
+
+New-Item -Path $ExportPath -ItemType Directory -Force | Out-Null
+
+function Get-FirstPropertyValue {
+    param(
+        [Parameter(Mandatory = $true)]
+        $Object,
+
+        [Parameter(Mandatory = $true)]
+        [string[]]$PropertyNames
+    )
+
+    foreach ($PropertyName in $PropertyNames) {
+        $Property = $Object.PSObject.Properties |
+        Where-Object { $_.Name -eq $PropertyName } |
+        Select-Object -First 1
+
+        if ($null -ne $Property) {
+            return $Property.Value
+        }
+    }
+
+    return $null
+}
+
 $RuleScopeReport = foreach ($Rule in Get-ADSyncRule | Sort-Object Direction, Precedence) {
 
-    $GroupNumber = 0
+    $ScopeGroupNumber = 0
 
     foreach ($ScopeGroup in @($Rule.ScopeFilter)) {
 
-        $GroupNumber++
+        $ScopeGroupNumber++
 
         $ConditionNumber = 0
 
@@ -16,29 +41,51 @@ $RuleScopeReport = foreach ($Rule in Get-ADSyncRule | Sort-Object Direction, Pre
 
             $ConditionNumber++
 
+            $RawProperties = (
+                $Condition.PSObject.Properties |
+                ForEach-Object {
+                    "$($_.Name)=$($_.Value)"
+                }
+            ) -join " | "
+
             [PSCustomObject]@{
-                RuleName         = $Rule.Name
-                RuleIdentifier   = $Rule.Identifier
-                Direction        = $Rule.Direction
-                Precedence       = $Rule.Precedence
-                Disabled         = $Rule.Disabled
-                Connector        = $Rule.Connector
-                SourceObjectType = $Rule.SourceObjectType
-                TargetObjectType = $Rule.TargetObjectType
+                RuleName               = $Rule.Name
+                RuleIdentifier         = $Rule.Identifier
+                Direction              = $Rule.Direction
+                Precedence             = $Rule.Precedence
+                Disabled               = $Rule.Disabled
+                Connector              = $Rule.Connector
+                SourceObjectType       = $Rule.SourceObjectType
+                TargetObjectType       = $Rule.TargetObjectType
 
-                ScopeGroup       = $GroupNumber
-                ConditionNumber  = $ConditionNumber
+                ScopeGroup             = $ScopeGroupNumber
+                ConditionNumber        = $ConditionNumber
 
-                Attribute        = $Condition.Attribute
-                Operator         = $Condition.ComparisonOperator
-                Value            = $Condition.ComparisonValue
+                Attribute              = Get-FirstPropertyValue -Object $Condition -PropertyNames @(
+                    "Attribute",
+                    "AttributeName",
+                    "SourceAttribute",
+                    "CSAttribute",
+                    "MVAttribute"
+                )
+
+                Operator               = Get-FirstPropertyValue -Object $Condition -PropertyNames @(
+                    "ComparisonOperator",
+                    "Operator",
+                    "ComparisonType"
+                )
+
+                Value                  = Get-FirstPropertyValue -Object $Condition -PropertyNames @(
+                    "ComparisonValue",
+                    "Value",
+                    "Values"
+                )
+
+                ConditionRawProperties = $RawProperties
             }
         }
     }
 }
 
 $RuleScopeReport |
-Export-Csv "$ExportPath\EntraConnect_SyncRules_ScopeFilters.csv" -NoTypeInformation -Encoding UTF8
-
-$RuleScopeReport |
-Format-Table RuleName, Direction, Precedence, ScopeGroup, Attribute, Operator, Value -AutoSize
+Export-Csv "$ExportPath\EntraConnect_SyncRules_ScopeFilters_WithRaw.csv" -NoTypeInformation -Encoding UTF8
